bae380a11db3c53e8e5e9a2922f6c8f2d1dd6641
Tomasz Bielawski <41069469+nilbu@users.noreply.github.com>
2026-04-09 14:14:55 +0200

    Update esp32_air_station.ino

--- firmware/esp32_air_station/esp32_air_station.ino
+++ firmware/esp32_air_station/esp32_air_station.ino
@@ -1,1 +1,21 @@
+/*
+  ESP32 Air Quality Station
+
+  Features:
+  - PMS5003 (PM1, PM2.5, PM10)
+  - BME280 (temperature, humidity, pressure)
+  - Median filter (PM)
+  - EMA filter (environmental data)
+  - Humidity correction
+  - Sensor duty cycle + clean cycle
+  - Google Sheets integration via HTTP
+
+  Hardware:
+  ESP32 DevKit
+  PMS5003 (UART)
+  BME280 (I2C)
+
+  Author: Tomek B
+*/
+
 
