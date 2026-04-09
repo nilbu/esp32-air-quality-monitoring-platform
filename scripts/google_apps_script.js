324c69e46e06609693ff64695011fe4cce80e713
Tomasz Bielawski <41069469+nilbu@users.noreply.github.com>
2026-04-09 14:51:26 +0200

    Update google_apps_script.js

--- scripts/google_apps_script.js
+++ scripts/google_apps_script.js
@@ -1,1 +1,18 @@
+function doGet(e) {
 
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1");
+
+  sheet.appendRow([
+    new Date(),                 // Device Time (zastępczo)
+    new Date(),                 // Logged Time
+    e.parameter.temp,
+    e.parameter.hum,
+    e.parameter.press,
+    e.parameter.pm1,
+    e.parameter.pm25,
+    e.parameter.pm10,
+    e.parameter.rssi
+  ]);
+
+  return ContentService.createTextOutput("OK");
+}
